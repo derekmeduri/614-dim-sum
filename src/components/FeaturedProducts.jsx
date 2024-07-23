8b6211ce02815d9f96a6adf0fa1c34e05ea47a78
Derek Meduri <derekmeduri@gmail.com>
2024-07-23 17:03:41 -0400

    updating fp

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -2,7 +2,7 @@ const FeaturedProducts = () => {
     return (
         <div className="container mx-auto pb-4 w-2/3">
             <h2>
-                Featured Products
+                
             </h2>
             
         </div>
